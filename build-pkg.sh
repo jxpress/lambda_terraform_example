bb5c68d8eda8963a0b9359063ffc5ffa06ca1832
suzukitaito <suzukitaito@suzukitaitonoMacBook-Pro.local>
2022-08-10 11:25:45 +0900

    依存パッケージの収集方法を洗練

--- build-pkg.sh
+++ build-pkg.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-PYTHON_VERSION="3.8"
 WORK_DIR="/tmp/lambda-pkg-$(date +%s)"
 DEPS_DIR="${WORK_DIR}/depends"
 DIST_DIR="${WORK_DIR}/dists"
@@ -12,9 +11,8 @@ mkdir $WORK_DIR
 mkdir $DEPS_DIR
 mkdir $DIST_DIR
 
-pipenv --python ${PYTHON_VERSION} install
-cp -r $(pipenv --venv)/lib/python${PYTHON_VERSION}/site-packages/* $DEPS_DIR/
-cp -r ${DEPS_DIR}/* ${DIST_DIR}/
+pipenv lock -r > requirements.txt
+pip install -r requirements.txt -t ${DIST_DIR}/
 cp -r ./functions/* ${DIST_DIR}/
 
 cd ${DIST_DIR}/ && zip -q -r dists.zip * && mv dists.zip ${OUT_DIR}/
